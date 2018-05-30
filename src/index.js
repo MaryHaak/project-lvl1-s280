74e7a285458d2f0d9f1dd4af65a98ec666e1e665
Mary <mariyahaak@gmail.com>
2018-05-31 00:25:27 +0300

    refactor (index.js) make code better

--- src/index.js
+++ src/index.js
@@ -1,41 +1,37 @@
 import readlineSync from 'readline-sync';
 
-
-const greetUser = () => {
-  console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!\n`);
-  return name;
-};
-
-
-const getUserAnswer = (num) => {
-  console.log(`Question: ${num}`);
+const getUserAnswer = (question) => {
+  console.log(`Question: ${question}`);
   return readlineSync.question('Your answer: ');
 };
 
+const getCorrectAnswer = question => (isEven(question) ? 'yes' : 'no');
 
-const getCorrectAnswer = num => (num % 2 === 0 ? 'yes' : 'no');
-
+const isEven = number => number % 2 === 0;
 
 const startGame = () => {
-  const name = greetUser();
+  console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!\n`);
 
-  for (let i = 0; i < 3; i += 1) {
-    const num = Math.floor(Math.random() * 100);
-    const correctAnswer = getCorrectAnswer(num);
-    const answer = getUserAnswer(num);
+  const levelsNum = 3;
 
+  for (let i = 0; i < levelsNum; i += 1) {
+    const question = Math.floor(Math.random() * 100);
+
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    const correctAnswer = getCorrectAnswer(question);
     if (answer === correctAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!\n`);
       return;
     }
   }
 
-  console.log(`Congratulations, ${name}!`);
+  console.log(`Congratulations, ${name}!\n`);
 };
-
 
 export default startGame;
